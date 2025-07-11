f0781ed1f15017b84af2f8c99a6e1d5483c412df
OrichalcosShunnoros <c3146092091@gmail.com>
2025-07-11 01:59:19 -0500

    Fix Tailwind styles in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    // Asegurarse de que los estilos CSS son procesados correctamente
+    css: {
+      postcss: {}
+    }
   }
 });
